f874a1e69f5e28af61246546b468ce7e8e1c0a2d
liubaolai <1164552059@qq.com>
2020-07-01 15:13:36 +0800

    first commit for test

--- cloud2020/cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
+++ cloud2020/cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
@@ -14,6 +14,9 @@ public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class, args);
     }
-
+    public String test(){
+        System.out.println("my name is liubaolai");
+        return "index";
+    }
 }
 
