426ff9749b3be0037baaaab02b029b1e73593df9
mengxin605 <mengxin605@163.com>
2020-07-02 14:34:59 +0800

    de

--- cloud2020/cloud-api-commons/src/main/java/com/atguigu/springcloud/entities/TestClass.java
+++
@@ -1,7 +0,0 @@
-package com.atguigu.springcloud.entities;
-
-public class TestClass {
-    public static void main(String args[]){
-        System.out.println("this class is just a test");
-    }
-}
