9e361520356767012e171d8a32f67e29e5e945db
lyskyfy <lyskyfy@gmail.com>
2020-08-10 16:19:43 +0800

    add config center module

---
+++ cloud2020/cloud-gateway-geteway9527/src/test/java/T2.java
@@ -0,0 +1,13 @@
+import java.time.ZonedDateTime;
+
+public class T2
+{
+    public static void main(String[] args) {
+
+        ZonedDateTime da = ZonedDateTime.now();
+        System.out.println(da);
+        // 2020-07-15T15:31:01.310+08:00[Asia/Shanghai]
+
+        System.out.println("This is a test");
+    }
+}
